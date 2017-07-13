0fc11e588aa881c290b5e91988af7a0622d2dc3c
cqtsjc <amith@cloud-quotient.com>
2017-07-13 23:46:00 +0530

    Update Command.java

--- src/entities/Command.java
+++ src/entities/Command.java
@@ -6,31 +6,17 @@ import java.io.InputStreamReader;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import com.google.gson.JsonObject;
-
 public class Command {
 	public static Response Run(Payload payload) 
     { 
-		System.out.println("In Command: Run()");
-		JsonObject jsonObject = new JsonObject();
-		String basePath = "/path/to/TESTS/";
-		String test = "FAIL";
+		String basePath = "/path/to/github/cloned/repository/";
+		String test = "";
         try 
         { 
-        	String cmd = "sh "+basePath+"run/test.sh "+basePath+"testbed/"+payload.Testbed+".json"+" "+basePath+"testCases/ProtocolSanity/"+payload.Protocol+".robot";
-        	System.out.println(cmd);
-            Process p=Runtime.getRuntime().exec(cmd); 
-            p.waitFor(); 
-            BufferedReader reader=new BufferedReader(
-                new InputStreamReader(p.getInputStream())
-            ); 
-            String line; 
-            while((line = reader.readLine()) != null) 
-            { 
-                System.out.println(line);
-            }
+        	String cmd = "/bin/sh "+basePath+"run/test.sh "+basePath+"testbed/"+payload.Testbed+".json"+" "+basePath+"testCases/"+payload.Protocol+".robot";
+        	Process p=Runtime.getRuntime().exec(cmd); 
             
-            test = "PASS";
+            test = "Command has been executed and script is running in the background";
             return Response.ok(test)
     				.header("Access-Control-Allow-Origin", "*")
     				.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
@@ -47,16 +33,7 @@ public class Command {
 				.header("Access-Control-Allow-Headers", "Content-Type")
 				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD" )
 				.allow("OPTIONS")
-				.build();} 
-        catch(InterruptedException e2) { test = "FAIL due to Keyboard interupt";
-        return Response.status(Status.NOT_FOUND)
-				.header("Access-Control-Allow-Origin", "*")
-				.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
-				.header("Access-Control-Allow-Headers", "Content-Type")
-				.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD" )
-				.allow("OPTIONS")
-				.build();} 
-
+				.build();}
+		} 
     }
 
-}
